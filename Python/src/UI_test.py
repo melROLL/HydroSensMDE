fe1fc9bc288eda6b9e8f16f1f1d83b9668226e62
melROLL <60772441+melROLL@users.noreply.github.com>
2023-02-17 11:57:15 +0100

    Update UI_test.py

--- Python/src/UI_test.py
+++ Python/src/UI_test.py
@@ -1,5 +1,19 @@
-# hello_world.py
-
 import PySimpleGUI as sg
+import os.path
 
 sg.Window(title="HydroSesMDE", layout=[[]], margins=(100, 50)).read()
+
+# First the window layout in 2 columns
+
+
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")
+    ],
+]
