4d1340926783a796b83d6e2841dd1688eddc7a22
melROLL <60772441+melROLL@users.noreply.github.com>
2023-02-19 00:03:48 +0100

    script updated

--- Python/src/camerascript.py
+++ Python/src/camerascript.py
@@ -1,32 +1,24 @@
 import cv2
-# pip3 install opencv-python
-
 import os
+# pip3 install opencv-python
 
 # create the folder if it doesn't exist
 if not os.path.exists("images"):
     os.makedirs("images")
-
-# select the device index for the camera you want to use
-# 0 is for the integrated webcam 
-device_index = 1
+    
+# select the device index for the camera you want to use 0 is the default webcam
+device_index = 0
 
 # initialize the camera
 cap = cv2.VideoCapture(device_index)
 
 # capture a frame
 ret, frame = cap.read()
-
-
-# capture a frame
-ret, frame = cap.read()
-
-# save the captured image to a file in the programefolder
-cv2.imwrite("image.jpg", frame)
-
+# save the captured image to a file
+cv2.imwrite("imagetest.jpg", frame)
 # save the captured image to a file in the images folder
+ret, frame = cap.read()
 cv2.imwrite("images/image.jpg", frame)
 
 # release the camera
 cap.release()
-
