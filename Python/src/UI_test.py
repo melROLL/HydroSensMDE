c290cf82ac765ea7163ae9e87b07162b153a05a5
Trixtax-ops <tristan.michelena@epfedu.fr>
2023-03-09 09:56:50 +0100

    Interface modification

--- Python/src/UI_test.py
+++ Python/src/UI_test.py
@@ -23,18 +23,31 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 3 
-        self.rows = 6
-        self.add_widget(Label(text="Name: ", size_hint_x=None, width=100))
-        self.add_widget(Label(text="John Doe"))
-        self.add_widget(Label(text="Age: "))
-        self.add_widget(Label(text="30"))
+        super(MyGrid, self).__init__(**kwargs)
+        # Set columns
+        self.cols = 1
+
+        # Set a second Gridlayout
+        self.top_grid = GridLayout()
+        self.top_grid.cols = 2
+
+        self.top_grid.add_widget(Label(text="Name of the sample: ")) #)), size_hint_x=None, width=500))
+        self.top_grid.add_widget(Label(text="John Doe"))
+        self.top_grid.add_widget(Label(text="Desired time between 2 pictures in seconds: "))
+        self.delta_T = TextInput(multiline=True)
+        self.top_grid.add_widget(self.delta_T)
+
+        # Add the new top_grid to our app
+        self.add_widget(self.top_grid)
+
         # Add a button
         self.add_widget(Button(text="Submit", on_press=self.submit_button_pressed, font_size=30, background_color=(3, 2, 4, 1)))
+
+
 
     def submit_button_pressed(self, button):
         print("Submit button pressed!")
