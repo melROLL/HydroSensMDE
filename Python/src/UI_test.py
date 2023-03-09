38b86272b058b72dbd96431254b604f1fe0bc928
Trixtax-ops <tristan.michelena@epfedu.fr>
2023-03-09 23:40:23 +0100

    New interface
    The brand new interface is here, very simple and user friendly.
    The main framework is built.

--- Python/src/UI_test.py
+++ Python/src/UI_test.py
@@ -17,7 +17,7 @@ file_list_column = [
             values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")
     ],
 ]"""
-
+"""
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -59,3 +59,97 @@ class MyApp(App):
 
 if __name__ == "__main__":
     MyApp().run()
+"""
+
+import os
+import tkinter as tk
+import customtkinter as ctk
+from tkinter import filedialog
+from PIL import ImageTk, Image
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+root = ctk.CTk()
+root.geometry("1280x720")
+
+def login():
+    print("Test")
+
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+# Use grid columnconfigure to make the first column resizable
+frame.columnconfigure(0, weight=1, minsize=200)
+frame.columnconfigure(1, weight=1, minsize=200)
+
+title = ctk.CTkLabel(master=frame, text="HydroSens System", font=("Helvetica", 24, "bold"))
+title.grid(row=0, column=0, columnspan=2, padx=20, pady=20)
+
+project_name = ctk.CTkEntry(master=frame, placeholder_text="Name of test project", font=("Helvetica", 14))
+project_name.grid(row=1, column=0, columnspan=2, padx=15, pady=10, sticky="ew")
+
+duration_between_pictures = ctk.CTkEntry(master=frame, placeholder_text="Number of seconds between each picture", font=("Helvetica", 14))
+duration_between_pictures.grid(row=2, column=0, columnspan=2, padx=15, pady=10, sticky="ew")
+
+#checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
+#checkbox.grid(row=3, columnspan=2, column=0, padx=15, pady=10, sticky="w")
+
+def open_folder():
+    folder_path = filedialog.askdirectory(title="Select an output folder")
+    if folder_path:
+        print(f"Selected folder: {folder_path}")
+        button.configure(text=str(folder_path), width=10, fg_color="purple", font=("Helvetica", 14))
+
+button = ctk.CTkButton(master=frame, text="Select an output folder",font=("Helvetica", 14), command=open_folder)
+button.grid(row=3, columnspan=2, column=0, padx=10, pady=10, sticky="ew")
+
+button_preview = ctk.CTkButton(master=frame, text="Preview", font=("Helvetica", 14), command=login)
+button_preview.grid(row=4, column=0, padx=10, pady=10, sticky="ew")
+
+button_execute = ctk.CTkButton(master=frame, text="Launch", font=("Helvetica", 14), command=login)
+button_execute.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
+
+image_container = ctk.CTkFrame(master=frame)
+image_container.grid(rowspan=4, row=5, columnspan=2, column=0, padx=10, pady=10, sticky="ns")
+
+# Get the path to the image file
+image_preview_path = os.path.join("..", "assets", "images", "image_preview.jpg")
+
+# Open the image and create a Tkinter-compatible PhotoImage object
+image = Image.open(image_preview_path)
+
+# Resize the image to a new width and height
+new_width = 500
+new_height = round(new_width*0.75)
+resized_image = image.resize((new_width, new_height))
+
+# Create a Tkinter-compatible PhotoImage object
+tk_image = ImageTk.PhotoImage(resized_image)
+
+# Create a Label widget and pack it inside the spacer frame
+label = tk.Label(image_container, image=tk_image)
+label.image = tk_image
+label.pack()
+
+# Use grid columnconfigure to make the first column resizable
+frame.columnconfigure(0, weight=1)
+
+root.mainloop()
+
+
+"""# sample code using Python and TensorFlo.py"""
+"""
+def open_image():
+    file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
+    if file_path:
+        image = Image.open(file_path)
+        image = image.resize((spacer.winfo_width(), spacer.winfo_height()))
+        tk_image = ImageTk.PhotoImage(image)
+        label = tk.Label(spacer, image=tk_image)
+        label.image = tk_image
+        label.pack()
+
+
+button = tk.Button(root, text="Open Image", command=open_image)
+button.pack()"""
