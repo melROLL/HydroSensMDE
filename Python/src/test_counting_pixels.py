4320bfcd979e794f360a80ae3d89236cd740c05a
melROLL <60772441+melROLL@users.noreply.github.com>
2023-04-14 18:25:48 +0100

    modified files

---
+++ Python/src/test_counting_pixels.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+
+#load the image 
+print("load the image")
+image = Image.open("image.jpg")
+image_np = np.array(image)
+
+#image to 1D array 
+image_flat = image_np.reshape(-1, 3)
+
+print(image_flat)
+
+#count the ocurence of each pixel by color
+unique_colours, counts = np.unique(image_flat, axis=0, return_counts=True)
+
+print("before counting")
+
+#display the result 
+for color, count in zip(unique_colours, counts):
+    print(f"Color: {color}, Count: {count}")
+
