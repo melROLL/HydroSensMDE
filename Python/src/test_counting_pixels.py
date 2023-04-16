37fd750e08cdc093fb985eeba966748de9e44eab
melROLL <60772441+melROLL@users.noreply.github.com>
2023-04-16 02:17:30 +0100

    Update test_counting_pixels.py

--- Python/src/test_counting_pixels.py
+++ Python/src/test_counting_pixels.py
@@ -16,3 +16,10 @@ unique_colours, counts = np.unique(image_flat, axis=0, return_counts=True)
 for color, count in zip(unique_colours, counts):
     print(f"Color: {color}, Count: {count}")
 
+# Calculate the total number of pixels in the image
+total_pixels = image_np.shape[0] * image_np.shape[1]
+
+# Calculate the percentage of each color
+for color, count in zip(unique_colours, counts):
+    percentage = (count / total_pixels) * 100
+    print(f"Color: {color}, Percentage: {percentage:.2f}%")
